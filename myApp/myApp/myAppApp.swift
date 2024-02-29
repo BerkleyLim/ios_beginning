4ae8510f0ec00aaa63be65bbf858a873aca09a1b
BerkleyLim <berkleylim16@gmail.com>
2024-02-29 21:30:08 +0900

    feat: create ios app - ios 프로젝트 생성하기 - 임시 입문용으로 사용

---
+++ myApp/myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Berkley on 2/29/24.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
